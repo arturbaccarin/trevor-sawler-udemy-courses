c99f7ed938a8daef07e5b62c6974a9bc6c6416dc
arturbaccarin <baccarin.artur@gmail.com>
2024-02-18 06:14:57 -0300

    29. Trying out our new addIPToContext middleware

---
+++ introduction-to-test-in-go/webapp/cmd/web/middleware_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func Test_application_addIPToContext(t *testing.T) {
+	tests := []struct {
+		headerName  string
+		headerValue string
+		addr        string
+		emptyAddr   bool
+	}{
+		{"", "", "", false},
+		{"", "", "", true},
+		{"X-Forwarded-For", "192.3.2.1", "", false},
+		{"", "", "hello:world", false},
+	}
+
+	// create a dummy handler
+	nextHandler := h
+}
