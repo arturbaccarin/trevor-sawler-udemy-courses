74908557554133f0b4682dea28c675d65edaa592
arturbaccarin <baccarin.artur@gmail.com>
2024-02-22 19:14:24 -0300

    33. Setting up a route and stub handler for the login form

---
+++ introduction-to-test-in-go/webapp/cmd/web/forms.go
@@ -0,0 +1,40 @@
+package main
+
+import "net/url"
+
+type errors map[string][]string
+
+func (e errors) Get(field string) string {
+	errorSlice := e[field]
+
+	if len(errorSlice) == 0 {
+		return ""
+	}
+
+	return errorSlice[0]
+}
+
+func (e errors) Add(field, message string) {
+	e[field] = append(e[field], message)
+}
+
+type Form struct {
+	Data   url.Values
+	Errors errors
+}
+
+func NewForm(data url.Values) *Form {
+	return &Form{
+		Data:   data,
+		Errors: map[string][]string{},
+	}
+}
+
+func (f *Form) Has(field string) bool {
+	x := f.Data.Get(field)
+	if x == "" {
+		return false
+	}
+
+	return true
+}
