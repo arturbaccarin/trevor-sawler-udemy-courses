660d5f63c0e48d3b264bd916d7a39eff029b3eea
arturbaccarin <baccarin.artur@gmail.com>
2024-02-24 08:12:47 -0300

    16. Adding a convenience method, UploadOneFile

--- building-a-module-in-go/toolkit-project/app-upload/main.go
+++ building-a-module-in-go/toolkit-project/app-upload/main.go
@@ -32,6 +32,7 @@ func routes() http.Handler {
 func uploadFiles(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
 	}
 
 	t := toolkit.Tools{
@@ -42,16 +43,33 @@ func uploadFiles(w http.ResponseWriter, r *http.Request) {
 	files, err := t.UploadFiles(r, "./uploads")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
 	}
 
 	out := ""
 	for _, item := range files {
-		out += fmt.Sprintf("Uploaded %s to the uploads folder, renamed to %s", item.OriginalFileName, item.NewFileName)
+		out += fmt.Sprintf("Uploaded %s to the uploads folder, renamed to %s\n", item.OriginalFileName, item.NewFileName)
 	}
 
 	_, _ = w.Write([]byte(out))
 }
 
 func uploadOneFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
+	t := toolkit.Tools{
+		MaxFileSize:      1024 * 1024 * 1024,
+		AllowedFileTypes: []string{"image/jpeg", "image/png", "image/gif"},
+	}
+
+	f, err := t.UploadOneFile(r, "./uploads")
+	if err != nil {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	_, _ = w.Write([]byte(fmt.Sprintf("Uploaded 1 file, %s, to the uploads folder", f.OriginalFileName)))
 }
--- building-a-module-in-go/toolkit-project/toolkit/tools_test.go
+++ building-a-module-in-go/toolkit-project/toolkit/tools_test.go
@@ -1,6 +1,8 @@
 package toolkit
 
 import (
+	"io"
+	"mime/multipart"
 	"testing"
 )
 
@@ -11,5 +13,22 @@ func TestTools_RandomString(t *testing.T) {
 
 	if len(s) != 10 {
 		t.Error("wrong lenght random string returned")
+	}
+}
+
+var uploadTests = []struct {
+	name          string
+	allowedTypes  []string
+	renameFile    bool
+	errorExpected error
+}{
+	{name: "allowed no rename", allowedTypes: []string{"image/jpeg", "image/png"}, renameFile: false, errorExpected: false}
+}
+
+func TestTools_UploadedFile(t *testing.T) {
+	for _, e := range uploadTests{
+		// setup a pipe to avoid buffering
+		pr, pw := io.Pipe()
+		writer := multipart.NewWriter(pw)
 	}
 }
