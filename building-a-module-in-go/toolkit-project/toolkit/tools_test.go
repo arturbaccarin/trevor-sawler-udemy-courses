0669c1dd2b077bf57f4a82982d5f089c9d830f79
arturbaccarin <baccarin.artur@gmail.com>
2024-02-25 07:18:49 -0300

    17. Writing a test for the UploadFiles method in our module

--- building-a-module-in-go/toolkit-project/toolkit/tools_test.go
+++ building-a-module-in-go/toolkit-project/toolkit/tools_test.go
@@ -1,8 +1,14 @@
 package toolkit
 
 import (
+	"fmt"
+	"image"
+	"image/png"
 	"io"
 	"mime/multipart"
+	"net/http/httptest"
+	"os"
+	"sync"
 	"testing"
 )
 
@@ -20,15 +26,71 @@ var uploadTests = []struct {
 	name          string
 	allowedTypes  []string
 	renameFile    bool
-	errorExpected error
+	errorExpected bool
 }{
-	{name: "allowed no rename", allowedTypes: []string{"image/jpeg", "image/png"}, renameFile: false, errorExpected: false}
+	{name: "allowed no rename", allowedTypes: []string{"image/jpeg", "image/png"}, renameFile: false, errorExpected: false},
 }
 
 func TestTools_UploadedFile(t *testing.T) {
-	for _, e := range uploadTests{
+	for _, e := range uploadTests {
 		// setup a pipe to avoid buffering
 		pr, pw := io.Pipe()
 		writer := multipart.NewWriter(pw)
+		var wg sync.WaitGroup
+		wg.Add(1)
+
+		go func() {
+			defer writer.Close()
+			defer wg.Done()
+
+			// Create the form date field 'file'
+			part, err := writer.CreateFormFile("file", "./testdata/img.png")
+			if err != nil {
+				t.Error(err)
+			}
+
+			f, err := os.Open("./testdata/img.png")
+			if err != nil {
+				t.Error(err)
+			}
+			defer f.Close()
+
+			img, _, err := image.Decode(f)
+			if err != nil {
+				t.Error("error decoding image", err)
+			}
+
+			err = png.Encode(part, img)
+			if err != nil {
+				t.Error(err)
+			}
+		}()
+
+		// read from the pipe which receive data
+		request := httptest.NewRequest("POST", "/", pr)
+		request.Header.Add("Content-Type", writer.FormDataContentType())
+
+		var testTools Tools
+		testTools.AllowedFileTypes = e.allowedTypes
+
+		uploadedFiles, err := testTools.UploadFiles(request, "./testdata/uploads/", e.renameFile)
+		if err != nil && !e.errorExpected {
+			t.Error(err)
+		}
+
+		if !e.errorExpected {
+			if _, err := os.Stat(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles[0].NewFileName)); os.IsNotExist(err) {
+				t.Errorf("%s: expected file to exist: %s", e.name, err.Error())
+			}
+
+			// clean up
+			_ = os.Remove(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles[0].NewFileName))
+		}
+
+		if !e.errorExpected && err != nil {
+			t.Errorf("%s: error expected but none received", e.name)
+		}
+
+		wg.Wait()
 	}
 }
