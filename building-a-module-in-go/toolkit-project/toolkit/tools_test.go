360403259c86ccb3f16567f6b44a2028cd79cca4
arturbaccarin <baccarin.artur@gmail.com>
2024-03-01 05:23:30 -0300

    29. Update our tests

--- building-a-module-in-go/toolkit-project/toolkit/tools_test.go
+++ building-a-module-in-go/toolkit-project/toolkit/tools_test.go
@@ -161,3 +161,31 @@ func TestTools_CreateDirIfNotExist(t *testing.T) {
 
 	_ = os.Remove("./testdata/myDir")
 }
+
+var slugTests = []struct {
+	name          string
+	s             string
+	expected      string
+	errorExpected bool
+}{
+	{name: "valid string", s: "now is the time", expected: "now-is-the-time", errorExpected: false},
+	{name: "empty string", s: "", expected: "", errorExpected: true},
+	{name: "complex string", s: "Now is the time for ALL GOOD men!", expected: "now-is-the-time-for-all-good-men", errorExpected: true},
+	{name: "japanese string", s: "三味線", expected: "", errorExpected: true},
+	{name: "japanese string and roman string", s: "hello-world-三味線", expected: "hello-world", errorExpected: true},
+}
+
+func TestTools_Slugify(t *testing.T) {
+	var testTools Tools
+
+	for _, e := range slugTests {
+		slug, err := testTools.Slugify(e.s)
+		if err != nil && !e.errorExpected {
+			t.Errorf("%s: error received when none expected: %s", e.name, err.Error())
+		}
+
+		if !e.errorExpected && slug != e.expected {
+			t.Errorf("%s: wrong slug returned; expected %s but got %s", e.name, e.expected, slug)
+		}
+	}
+}
