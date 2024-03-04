5d07ca39d3b69dadcd01ee4e5d87e3964ba20354
arturbaccarin <baccarin.artur@gmail.com>
2024-03-04 04:42:20 -0300

    34. Writing a test for DownloadStaticFile

---
+++ building-a-module-in-go/toolkit-project/app-download/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/arturbaccarin/toolkit"
+)
+
+func main() {
+	// get router
+	mux := routes()
+
+	// start a server
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func routes() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/download", downloadFile)
+
+	return mux
+}
+
+func downloadFile(w http.ResponseWriter, r *http.Request) {
+	t := toolkit.Tools{}
+
+	t.DownloadStaticFile(w, r, "./files", "img.png", "ball.jpg")
+}
