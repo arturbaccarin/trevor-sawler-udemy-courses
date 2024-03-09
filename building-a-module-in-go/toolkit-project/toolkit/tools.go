7d3d2d2d4b535e73a2bf03719b698deadf44a8b0
arturbaccarin <baccarin.artur@gmail.com>
2024-03-09 11:20:23 -0300

    44. Writing a test for ErrorJSON

--- building-a-module-in-go/toolkit-project/toolkit/tools.go
+++ building-a-module-in-go/toolkit-project/toolkit/tools.go
@@ -264,3 +264,41 @@ func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data any) error
 
 	return nil
 }
+
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	_, err = w.Write(out)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (t *Tools) ErrorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+
+	if len(status) > 0 {
+		statusCode = status[0]
+	}
+
+	var payload JSONResponse
+	payload.Error = true
+	payload.Message = err.Error()
+
+	return t.WriteJSON(w, statusCode, payload)
+}
