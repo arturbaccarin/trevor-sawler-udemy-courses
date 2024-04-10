75b0780f4bd0b37bd33f799575134c3f25d948d3
arturbaccarin <baccarin.artur@gmail.com>
2024-04-10 06:27:33 -0300

    97. Trying out the authentication handler

--- introduction-to-test-in-go/webapp/cmd/api/auth.go
+++ introduction-to-test-in-go/webapp/cmd/api/auth.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"strings"
 	"time"
+	"webapp/pkg/data"
 
 	"github.com/golang-jwt/jwt/v4"
 )
@@ -14,8 +15,8 @@ const jwtTokenExpiry = time.Minute * 15
 const refreshTokenExpiry = time.Hour * 24
 
 type TokenPairs struct {
-	Token   string `json:"access_token`
-	Refresh string `json:"refresh_token`
+	Token        string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
 }
 
 type Claims struct {
@@ -23,7 +24,7 @@ type Claims struct {
 	jwt.RegisteredClaims
 }
 
-func (app *application) getTokenFromHeaderandVerify(w http.ResponseWriter, r *http.Request) (string, *Claims, error) {
+func (app *application) getTokenFromHeaderAndVerify(w http.ResponseWriter, r *http.Request) (string, *Claims, error) {
 	// add header
 	w.Header().Add("Vary", "Authorization")
 
@@ -76,4 +77,52 @@ func (app *application) getTokenFromHeaderandVerify(w http.ResponseWriter, r *ht
 
 	// valid token
 	return token, claims, nil
+}
+
+func (app *application) generateTokenPair(user *data.User) (TokenPairs, error) {
+	// Create the token
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	// set claims
+	claims := token.Claims.(jwt.MapClaims)
+	claims["name"] = fmt.Sprintf("%s %s", user.FirstName, user.LastName)
+	claims["sub"] = fmt.Sprint(user.ID)
+	claims["aud"] = app.Domain
+	claims["iss"] = app.Domain
+
+	if user.IsAdmin == 1 {
+		claims["admin"] = true
+	} else {
+		claims["admin"] = false
+	}
+
+	// set the expiry
+	claims["exp"] = time.Now().Add(jwtTokenExpiry).Unix()
+
+	// create the signed token
+	signedAccessToken, err := token.SignedString([]byte(app.JWTSecret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	// create the refresh token
+	refreshToken := jwt.New(jwt.SigningMethodHS256)
+	refreshTokenClaims := refreshToken.Claims.(jwt.MapClaims)
+	refreshTokenClaims["sub"] = fmt.Sprint(user.ID)
+
+	// set expiry; must be longer than jwt expiry
+	refreshTokenClaims["exp"] = time.Now().Add(refreshTokenExpiry).Unix()
+
+	// create signed refresh token
+	signedRefreshToken, err := refreshToken.SignedString([]byte(app.JWTSecret))
+	if err != nil {
+		return TokenPairs{}, err
+	}
+
+	var tokenPairs = TokenPairs{
+		Token:        signedAccessToken,
+		RefreshToken: signedRefreshToken,
+	}
+
+	return tokenPairs, nil
 }
