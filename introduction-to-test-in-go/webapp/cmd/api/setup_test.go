57e9af67652a1ae1e0e3a6cc0a39f80b29405c5c
arturbaccarin <baccarin.artur@gmail.com>
2024-04-12 04:58:29 -0300

    100. Testing generating and validating tokens

--- introduction-to-test-in-go/webapp/cmd/api/setup_test.go
+++ introduction-to-test-in-go/webapp/cmd/api/setup_test.go
@@ -7,6 +7,7 @@ import (
 )
 
 var app application
+var expiredToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZG1pbiI6dHJ1ZSwiYXVkIjoiZXhhbXBsZS5jb20iLCJleHAiOjE3MTI1NDc2NTQsImlzcyI6ImV4YW1wbGUuY29tIiwibmFtZSI6IkpvaG4gRG9lIiwic3ViIjoiMSJ9.5zfui_Ya8d1mUcBpIPpeZ-xr1407XUy9fd2poD16nfY"
 
 func TestMain(m *testing.M) {
 	app.DB = &dbrepo.TestDBRepo{}
