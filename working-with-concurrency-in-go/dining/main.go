33f98d895b752e21bfba81a7d1fdf2cc6dc7ad36
arturbaccarin <baccarin.artur@gmail.com>
2024-06-07 08:21:11 -0300

    27. Getting started with the problem

--- working-with-concurrency-in-go/dining/main.go
+++ working-with-concurrency-in-go/dining/main.go
@@ -1,1 +1,65 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type Philosopher struct {
+	name      string
+	rightFork int
+	leftFork  int
+}
+
+var philosophers = []Philosopher{
+	{name: "Plato", leftFork: 4, rightFork: 0},
+	{name: "Socrates", leftFork: 0, rightFork: 1},
+	{name: "Aristotle", leftFork: 1, rightFork: 2},
+	{name: "Pascal", leftFork: 2, rightFork: 3},
+	{name: "Locke", leftFork: 3, rightFork: 0},
+}
+
+var hunger = 3 // how many times does a person eat?
+var eatTime = 1 * time.Second
+var thinkTime = 3 * time.Second
+var sleepTime = 1 * time.Second
+
+func main() {
+	// print out a welcome message
+	fmt.Println("Dining Philosophers Problem")
+	fmt.Println("---------------------------")
+	fmt.Println("The table is empty.")
+
+	// start the meal
+	dine()
+
+	// print out finished message
+	fmt.Println("The table is empty.")
+}
+
+func dine() {
+	wg := &sync.WaitGroup{}
+	wg.Add(len(philosophers))
+
+	seated := &sync.WaitGroup{}
+	seated.Add(len(philosophers))
+
+	// forks is a map of all 5 forks.
+	var forks = make(map[int]*sync.Mutex)
+	for i := 0; i < len(philosophers); i++ {
+		forks[i] = &sync.Mutex{}
+	}
+
+	// start the meal.
+	for i := 0; i < len(philosophers); i++ {
+		// fire off a goroutine for the current philosopher
+		go diningProblem(philosophers[i], wg, forks, seated)
+	}
+
+	wg.Wait()
+}
+
+func diningProblem(philosopher Philosopher, wg *sync.WaitGroup, forks map[int]*sync.Mutex, seated *sync.WaitGroup) {
+	defer wg.Done()
+}
