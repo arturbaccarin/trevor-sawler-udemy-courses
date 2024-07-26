564f3c3d9297889492a6b7f79bbcbd943cc13c9a
arturbaccarin <baccarin.artur@gmail.com>
2024-07-26 07:38:20 -0300

    77. Trying things out, subscribing a user, updating the session, and redirecting

--- working-with-concurrency-in-go/final-project/cmd/web/handlers.go
+++ working-with-concurrency-in-go/final-project/cmd/web/handlers.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"final-project/data"
 	"fmt"
 	"html/template"
@@ -36,7 +35,7 @@ func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
 
 	user, err := app.Models.User.GetByEmail(email)
 	if err != nil {
-		app.Session.Put(r.Context(), "error", "invalid credentials")
+		app.Session.Put(r.Context(), "error", "Invalid credentials.")
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
 		return
 	}
@@ -44,7 +43,7 @@ func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
 	// check password
 	validPassword, err := user.PasswordMatches(password)
 	if err != nil {
-		app.Session.Put(r.Context(), "error", "invalid credentials")
+		app.Session.Put(r.Context(), "error", "Invalid credentials.")
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
 		return
 	}
@@ -52,12 +51,12 @@ func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
 	if !validPassword {
 		msg := Message{
 			To:      email,
-			Subject: "Failed to login attempt",
-			Data:    "Invalid login attempt",
+			Subject: "Failed log in attempt",
+			Data:    "Invalid login attempt!",
 		}
 
 		app.sendEmail(msg)
-		app.Session.Put(r.Context(), "error", "invalid credentials")
+		app.Session.Put(r.Context(), "error", "Invalid credentials.")
 		http.Redirect(w, r, "/login", http.StatusSeeOther)
 		return
 	}
@@ -66,7 +65,8 @@ func (app *Config) PostLoginPage(w http.ResponseWriter, r *http.Request) {
 	app.Session.Put(r.Context(), "userID", user.ID)
 	app.Session.Put(r.Context(), "user", user)
 
-	app.Session.Put(r.Context(), "flash", "succesful login")
+	app.Session.Put(r.Context(), "flash", "Successful login!")
+
 	// redirect the user
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
@@ -91,11 +91,11 @@ func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
 
 	// TODO - validate data
 
-	// create an user
+	// create a user
 	u := data.User{
 		Email:     r.Form.Get("email"),
-		FirstName: r.Form.Get("first_name"),
-		LastName:  r.Form.Get("last_name"),
+		FirstName: r.Form.Get("first-name"),
+		LastName:  r.Form.Get("last-name"),
 		Password:  r.Form.Get("password"),
 		Active:    0,
 		IsAdmin:   0,
@@ -103,7 +103,7 @@ func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
 
 	_, err = u.Insert(u)
 	if err != nil {
-		app.Session.Put(r.Context(), "error", "unable to create an user")
+		app.Session.Put(r.Context(), "error", "Unable to create user.")
 		http.Redirect(w, r, "/register", http.StatusSeeOther)
 		return
 	}
@@ -115,27 +115,25 @@ func (app *Config) PostRegisterPage(w http.ResponseWriter, r *http.Request) {
 
 	msg := Message{
 		To:       u.Email,
-		Subject:  "activate yout account",
+		Subject:  "Activate your account",
 		Template: "confirmation-email",
 		Data:     template.HTML(signedURL),
 	}
 
 	app.sendEmail(msg)
 
-	app.Session.Put(r.Context(), "flash", "Please check your email")
+	app.Session.Put(r.Context(), "flash", "Confirmation email sent. Check your email.")
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
-
 }
 
 func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
 	// validate url
 	url := r.RequestURI
 	testURL := fmt.Sprintf("http://localhost%s", url)
-
 	okay := VerifyToken(testURL)
 
 	if !okay {
-		app.Session.Put(r.Context(), "error", "Invalid token")
+		app.Session.Put(r.Context(), "error", "Invalid token.")
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
@@ -143,7 +141,7 @@ func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
 	// activate account
 	u, err := app.Models.User.GetByEmail(r.URL.Query().Get("email"))
 	if err != nil {
-		app.Session.Put(r.Context(), "error", "Invalid token")
+		app.Session.Put(r.Context(), "error", "No user found.")
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
@@ -151,31 +149,28 @@ func (app *Config) ActivateAccount(w http.ResponseWriter, r *http.Request) {
 	u.Active = 1
 	err = u.Update()
 	if err != nil {
-		app.Session.Put(r.Context(), "error", "unable to update user.")
+		app.Session.Put(r.Context(), "error", "Unable to update user.")
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
 	app.Session.Put(r.Context(), "flash", "Account activated. You can now log in.")
 	http.Redirect(w, r, "/login", http.StatusSeeOther)
-
-	// genereate an invoice
-
-	// send an email with attachments
-
-	// subscribe the user to an account
 }
 
 func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 	// get the id of the plan that is chosen
 	id := r.URL.Query().Get("id")
 
-	planID, _ := strconv.Atoi(id)
+	planID, err := strconv.Atoi(id)
+	if err != nil {
+		app.ErrorLog.Println("Error getting planid:", err)
+	}
 
 	// get the plan from the database
 	plan, err := app.Models.Plan.GetOne(planID)
 	if err != nil {
-		app.Session.Put(r.Context(), "error", "Unable to find plan")
+		app.Session.Put(r.Context(), "error", "Unable to find plan.")
 		http.Redirect(w, r, "/members/plans", http.StatusSeeOther)
 		return
 	}
@@ -209,13 +204,12 @@ func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 		app.sendEmail(msg)
 	}()
 
-	// generate manual
+	// generate a manual
 	app.Wait.Add(1)
 	go func() {
 		defer app.Wait.Done()
 
 		pdf := app.generateManual(user, plan)
-
 		err := pdf.OutputFileAndClose(fmt.Sprintf("./tmp/%d_manual.pdf", user.ID))
 		if err != nil {
 			app.ErrorChan <- err
@@ -225,19 +219,31 @@ func (app *Config) SubscribeToPlan(w http.ResponseWriter, r *http.Request) {
 		msg := Message{
 			To:      user.Email,
 			Subject: "Your manual",
-			Data:    "Your user manual is attached.",
+			Data:    "Your user manual is attached",
 			AttachmentMap: map[string]string{
 				"Manual.pdf": fmt.Sprintf("./tmp/%d_manual.pdf", user.ID),
 			},
 		}
 
 		app.sendEmail(msg)
-
-		// test app error chan
-		app.ErrorChan <- errors.New("some custom error")
 	}()
 
-	// subscribe the user to an account
+	// subscribe the user to a plan
+	err = app.Models.Plan.SubscribeUserToPlan(user, *plan)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Error subscribing to plan!")
+		http.Redirect(w, r, "/members/plans", http.StatusSeeOther)
+		return
+	}
+
+	u, err := app.Models.User.GetOne(user.ID)
+	if err != nil {
+		app.Session.Put(r.Context(), "error", "Error getting user from database!")
+		http.Redirect(w, r, "/members/plan", http.StatusSeeOther)
+		return
+	}
+
+	app.Session.Put(r.Context(), "user", u)
 
 	// redirect
 	app.Session.Put(r.Context(), "flash", "Subscribed!")
@@ -263,7 +269,7 @@ func (app *Config) generateManual(u data.User, plan *data.Plan) *gofpdf.Fpdf {
 	pdf.SetFont("Arial", "", 12)
 	pdf.MultiCell(0, 4, fmt.Sprintf("%s %s", u.FirstName, u.LastName), "", "C", false)
 	pdf.Ln(5)
-	pdf.MultiCell(0, 4, fmt.Sprintf("%s User Guid"), "", "C", false)
+	pdf.MultiCell(0, 4, fmt.Sprintf("%s User Guide", plan.PlanName), "", "C", false)
 
 	return pdf
 }
